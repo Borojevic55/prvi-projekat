d9c9a79c529f3035851f9f40b4577e29f07c532a
Your Name <you@example.com>
2023-02-03 10:50:07 +0100

    dodao dom

---
+++ dom.js
@@ -0,0 +1,10 @@
+//console.dir(document)
+//console.log(document.domain)
+//console.log(document.title)
+document.title="moj prvi sajt"
+///console.log(document.body)
+
+let items=document.getElementById("items")
+//console.log(items)
+let listItems=document.getElementsByClassName("list-items")
+listItems[2].textContent="djuka jarace"
